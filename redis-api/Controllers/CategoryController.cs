318470cf07b04d79a672faca763e7aaa9fe52c6f
Fernando Cabral <fernandocabral517@gmail.com>
2024-03-04 18:25:25 -0300

    Project ASPNET CORE With Redis Cache

---
+++ redis-api/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+using System;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace redis_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IDistributedCache _cache;
+        private string key = "Category";
+
+        public CategoryController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ResponseModel>> Get()
+        {
+            var categorys = new List<string>();
+
+            var cacheList = await _cache.GetAsync(key);
+
+            if (cacheList is not null)
+            {
+                categorys = JsonSerializer.Deserialize<List<string>>(cacheList);
+            }
+
+            return new List<ResponseModel>
+            {
+                new ResponseModel
+                {
+                    status = 200,
+                    errorMessage = string.Empty,
+                    result = categorys!
+                }
+            };
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] string value)
+        { 
+            var categorys = new List<string>();
+
+            var cacheList = await _cache.GetAsync(key);
+
+            if(cacheList is not null)
+            {
+                categorys = JsonSerializer.Deserialize<List<string>>(cacheList);
+            }
+
+            categorys?.Add(value);
+
+            var serialized = JsonSerializer.Serialize(categorys);
+
+            var redisList = Encoding.UTF8.GetBytes(serialized);
+
+            var options = new DistributedCacheEntryOptions()
+                  .SetAbsoluteExpiration(DateTime.Now.AddMinutes(10))
+                  .SetSlidingExpiration(TimeSpan.FromMinutes(2));
+
+            await _cache.SetAsync(key, redisList, options);
+        }
+    }
+}
---
+++ redis-api/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace redis_api
+{
+    public class ResponseModel
+    {
+        public object result { get; set; }
+        public int status { get; set; }
+        public string errorMessage { get; set; }
+    }
+}
